3151611590e9be036e292dbd5e4960bf745ec72b
Super <rivierabo@gmail.com>
2021-08-22 20:54:42 -0300

    actualizado el app.post 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('./db/mongoose');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-const Dish = require('./model/games');
+const Game = require('./model/games');
 
 app.use(express.json());
 
